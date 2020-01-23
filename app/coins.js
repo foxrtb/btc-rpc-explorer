7077e50eb5726e1a315c20dd37c88e2c09a27388
FoxD <foxrtb@gmail.com>
2020-01-23 11:12:23 +0200

    rebranding

--- app/coins.js
+++ app/coins.js
@@ -1,9 +1,11 @@
 var btc = require("./coins/btc.js");
 var ltc = require("./coins/ltc.js");
+var ltc = require("./coins/mil.js");
 
 module.exports = {
 	"BTC": btc,
 	"LTC": ltc,
+	"MIL": mil,
 
-	"coins":["BTC", "LTC"]
+	"coins":["BTC", "LTC", "MIL"]
 };
